1e53deac34d23738545271cc6ad7b8a8276aec62
benoit74 <benoit74@users.noreply.github.com>
2024-11-08 15:47:04 +0000

    Remove pyhumps, pydantic already knows how to camelize

--- scraper/src/mindtouch2zim/ui.py
+++ scraper/src/mindtouch2zim/ui.py
@@ -1,11 +1,11 @@
-from humps import camelize
 from pydantic import BaseModel, ConfigDict
+from pydantic.alias_generators import to_camel
 
 
 class CamelModel(BaseModel):
     """Model to transform Python snake_case into JSON camelCase."""
 
-    model_config = ConfigDict(alias_generator=camelize, populate_by_name=True)
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
 
 
 class PageModel(CamelModel):
